0713b34f059516637d28c0a953161844ee788193
hyunhee1875-prog <hyunhee1875@gmail.com>
2026-04-21 10:11:29 +0900

    license

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://cvpxlkjzpdhovxybcyfd.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_cs4qrSmN12JELCVErv-Epw_ces6DLEK";
+const SUPABASE_URL = "https://wbtlkjrcynjorxweqkig.supabase.co/rest/v1/";
+const SUPABASE_ANON_KEY = "sb_publishable_oHs7jUdsBiXFdx2JQPIfzw_7HilAwi-";
 
 window.sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
