f664650679e62e4bd04a569fbad70688040a2e88
hyunhee1875-prog <hyunhee1875@gmail.com>
2026-04-21 10:35:00 +0900

    Fianl

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://wbtlkjrcynjorxweqkig.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://wbtlkjrcynjorxweqkig.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_oHs7jUdsBiXFdx2JQPIfzw_7HilAwi-";
 
 window.sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
